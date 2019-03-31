9662057c14fe03900655c9c948d9d24d9264a903
Samuel <samudomingos2018@gmail.com>
2019-03-31 15:50:50 -0300

    Criei a workspace e dei ínicio ao projeto

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+require 'pages'.DIRECTORY_SEPARATOR.'header.php';
+
+?>
+<div class="container-fluid">
+    <div class="jumbotron mt-3">
+        <h2>Nós temos hoje 999 anúncios.</h2>
+        <p>E mais de 999 usuários cadastrados.</p>
+    </div>
+    <div class="row">
+        <div class="col-sm-3">
+            <h4>Pesquisa Avançada</h4>
+        </div>
+        <div class="col-sm-9">
+            <h4>Últimos Anúncios</h4>
+        </div>
+    </div>
+</div>
+    <script type="text/javascript" src="assets/js/jquery.min.js"></script>
+    <script type="text/javascript" src="assets/js/bootstrap.bundle.min.js"></script>
+    <script type="text/javascript" src="assets/js/script.js"></script>
+<?php
+require 'pages'.DIRECTORY_SEPARATOR.'footer.php';
+
+?>
